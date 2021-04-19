f6ede919d46e88518b6c395a2f60dbfc694060ff
PHBMB <58255326+PHBMB@users.noreply.github.com>
2021-04-19 21:15:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy
 import matplotlib.pyplot as pyplot
 from sklearn.metrics import r2_score
 
-# data for tzhe model
+# data for the model
 x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
 
